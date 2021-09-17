4d4bc5bd700063d6b3a2859ccced4b5770ca19b6
Terry D. Eppler <terry.eppler@gmail.com>
2021-09-17 09:22:53 -0500

    added StartUp form

---
+++ forms/StartUp.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file=" <File Name> .cs" company="Terry D. Eppler">
+// Copyright (c) Terry Eppler. All rights reserved.
+// </copyright>
+//
+
+namespace BudgetExcess
+{
+    using System;
+    using Syncfusion.Windows.Forms;
+    using System.Windows.Forms;
+
+    public partial class StartUp : MetroForm
+    {
+        public StartUp()
+        {
+            InitializeComponent();
+        }
+    }
+}
